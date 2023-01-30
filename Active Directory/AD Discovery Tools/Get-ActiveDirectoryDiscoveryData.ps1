fe0f714dc87c7ef5a18144e718226998ccbd7327
Charles Coulter <charlesjcoulter@gmail.com>
2023-01-30 14:37:21 -0500

    more additions

--- Active Directory/AD Discovery Tools/Get-ActiveDirectoryDiscoveryData.ps1
+++ Active Directory/AD Discovery Tools/Get-ActiveDirectoryDiscoveryData.ps1
@@ -310,6 +310,7 @@ function ADDR {
     Write-Host -ForegroundColor Cyan        "# 2. Network Configuration                                                 #"
     Write-Host -ForegroundColor Cyan        "# 3. Installed Roles and Features                                          #"
     Write-Host -ForegroundColor Cyan        "# 4. List of Installed Applications                                        #"
+    Write-Host -ForegroundColor Cyan        "# 5. List of DNS Forwarders                                                #"
     Write-Host -ForegroundColor Green       "############################################################################" 
 
     foreach ($i in $DomainControllers.hostname)
@@ -324,6 +325,7 @@ function ADDR {
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        
         Write-Host -ForegroundColor Green "Starting collection of network configuration details on " -NoNewline;
         Write-Host -ForegroundColor Yellow $i
         "#######$i NETWORK COLLECTION START########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
@@ -335,13 +337,37 @@ function ADDR {
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        
         Write-Host -ForegroundColor Green "Starting collection of installed Roles and Features on " -NoNewline;
         Write-Host -ForegroundColor Yellow $i
-        "#######$i NETWORK INSTALLED ROLES AND FEATURES START########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        "#######$i INSTALLED ROLES AND FEATURES START########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         $InstalledRolesAndFeatures = Get-WindowsFeature | Where-Object {$_.installstate -eq "Installed"} | Out-String
         Write-Host -ForegroundColor Yellow $InstalledRolesAndFeatures
         $InstalledRolesAndFeatures | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
-        "#######$i NETWORK INSTALLED ROLES AND FEATURES COMPLETE########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        "#######$i INSTALLED ROLES AND FEATURES COMPLETE########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+
+        Write-Host -ForegroundColor Green "Starting collection of installed software on " -NoNewline;
+        Write-Host -ForegroundColor Yellow $i
+        "#######$i INSTALLED APPLICATIONS START########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        $InstalledSoftware = Get-InstalledSoftware | Out-String
+        Write-Host -ForegroundColor Yellow $InstalledSoftware
+        $InstalledSoftware |  Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        "#######$i INSTALLED APPLICATIONS COMPLETE########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        " " | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+
+
+        Write-Host -ForegroundColor Green "Starting collection of DNS Forwarders on " -NoNewline;
+        Write-Host -ForegroundColor Yellow $i
+        "#######$i DNS FORWARDERS START########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        $DNSForwarders = Get-DnsServerForwarder | Out-String
+        Write-Host -ForegroundColor Yellow $DNSForwarders 
+        $DNSForwarders | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
+        "#######$i DNS FORWARDERS COMPLETE########" | Add-Content -Path "$ReportPath\$i-DomainControllerDiscoveryData.log"
         Exit-PSSession
 
 
@@ -353,7 +379,109 @@ function ADDR {
     Pause
     BasicADHCMenu
 }   
-
+function Get-InstalledSoftware {
+    <#
+	.SYNOPSIS
+		Retrieves a list of all software installed on a Windows computer.
+	.EXAMPLE
+		PS> Get-InstalledSoftware
+		
+		This example retrieves all software installed on the local computer.
+	.PARAMETER ComputerName
+		If querying a remote computer, use the computer name here.
+	
+	.PARAMETER Name
+		The software title you'd like to limit the query to.
+	
+	.PARAMETER Guid
+		The software GUID you'e like to limit the query to
+	#>
+    [CmdletBinding()]
+    param (
+		
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$ComputerName = $env:COMPUTERNAME,
+		
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [string]$Name,
+		
+        [Parameter()]
+        [guid]$Guid
+    )
+    process {
+        try {
+            $scriptBlock = {
+                $args[0].GetEnumerator() | ForEach-Object { New-Variable -Name $_.Key -Value $_.Value }
+				
+                $UninstallKeys = @(
+                    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall",
+                    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+                )
+                New-PSDrive -Name HKU -PSProvider Registry -Root Registry::HKEY_USERS | Out-Null
+                $UninstallKeys += Get-ChildItem HKU: | where { $_.Name -match 'S-\d-\d+-(\d+-){1,14}\d+$' } | foreach {
+                    "HKU:\$($_.PSChildName)\Software\Microsoft\Windows\CurrentVersion\Uninstall"
+                }
+                if (-not $UninstallKeys) {
+                    Write-Warning -Message 'No software registry keys found'
+                } else {
+                    foreach ($UninstallKey in $UninstallKeys) {
+                        $friendlyNames = @{
+                            'DisplayName'    = 'Name'
+                            'DisplayVersion' = 'Version'
+                        }
+                        Write-Verbose -Message "Checking uninstall key [$($UninstallKey)]"
+                        if ($Name) {
+                            $WhereBlock = { $_.GetValue('DisplayName') -like "$Name*" }
+                        } elseif ($GUID) {
+                            $WhereBlock = { $_.PsChildName -eq $Guid.Guid }
+                        } else {
+                            $WhereBlock = { $_.GetValue('DisplayName') }
+                        }
+                        $SwKeys = Get-ChildItem -Path $UninstallKey -ErrorAction SilentlyContinue | Where-Object $WhereBlock
+                        if (-not $SwKeys) {
+                            Write-Verbose -Message "No software keys in uninstall key $UninstallKey"
+                        } else {
+                            foreach ($SwKey in $SwKeys) {
+                                $output = @{ }
+                                foreach ($ValName in $SwKey.GetValueNames()) {
+                                    if ($ValName -ne 'Version') {
+                                        $output.InstallLocation = ''
+                                        if ($ValName -eq 'InstallLocation' -and 
+                                            ($SwKey.GetValue($ValName)) -and 
+                                            (@('C:', 'C:\Windows', 'C:\Windows\System32', 'C:\Windows\SysWOW64') -notcontains $SwKey.GetValue($ValName).TrimEnd('\'))) {
+                                            $output.InstallLocation = $SwKey.GetValue($ValName).TrimEnd('\')
+                                        }
+                                        [string]$ValData = $SwKey.GetValue($ValName)
+                                        if ($friendlyNames[$ValName]) {
+                                            $output[$friendlyNames[$ValName]] = $ValData.Trim() ## Some registry values have trailing spaces.
+                                        } else {
+                                            $output[$ValName] = $ValData.Trim() ## Some registry values trailing spaces
+                                        }
+                                    }
+                                }
+                                $output.GUID = ''
+                                if ($SwKey.PSChildName -match '\b[A-F0-9]{8}(?:-[A-F0-9]{4}){3}-[A-F0-9]{12}\b') {
+                                    $output.GUID = $SwKey.PSChildName
+                                }
+                                New-Object -TypeName PSObject -Prop $output
+                            }
+                        }
+                    }
+                }
+            }
+			
+            if ($ComputerName -eq $env:COMPUTERNAME) {
+                & $scriptBlock $PSBoundParameters
+            } else {
+                Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $PSBoundParameters
+            }
+        } catch {
+            Write-Error -Message "Error: $($_.Exception.Message) - Line Number: $($_.InvocationInfo.ScriptLineNumber)"
+        }
+    }
+}
 
 Function ADDMENU {
     Clear-Host
