2aa65ce7f5f232a123399e54341e0be486601037
Charles Coulter <charlesjcoulter@gmail.com>
2023-02-16 10:32:14 -0500

    added test script for Record generation

--- DNS/generate-testrecords.ps1
+++ DNS/generate-testrecords.ps1
@@ -1,11 +1,13 @@
+Import-Module NameIT
 $oct = 1
 
-Measure-Command { 1..200 | ForEach-Object {
+Measure-Command { 1..100 | ForEach-Object {
 
-$name = Invoke-Generate "APC-???-####"
-$ip = "10.10.16.$oct"
-$oct++
-Add-DnsServerResourceRecordA -ZoneName "catuslab.local" -CreatePtr -Name $name -Verbose -IPv4Address $ip -AgeRecord
+    $name = Invoke-Generate "APC-???-####"
+    $ip = "10.10.11.$oct"
+    $oct++
+        Add-DnsServerResourceRecordA -ZoneName "catuslab.local" -CreatePtr -Name $name -Verbose -IPv4Address $ip -AgeRecord
 
+    }
 }
-}
+
