879a66cbd78e3786db188b7356f64a2b76d57596
Charles Coulter <charlesjcoulter@gmail.com>
2023-05-15 09:37:22 -0400

    added log cleanup

--- DHCP/get-DHCPServerActiveScopeReport.ps1
+++ DHCP/get-DHCPServerActiveScopeReport.ps1
@@ -11,13 +11,13 @@
 $dhcpServer = "$env:COMPUTERNAME"
 
 # Define the email parameters
-$smtpServer = "smtp.office365.com"
-$smtpPort = 587
-$smtpFrom = "cj@melillodemo.com"
-$smtpTo = "Charlesjcoulter@gmail.com"
+$smtpServer = "relayhost.hhsnjsmtp.com"
+$smtpFrom = "DHCPAlert-$env:COMPUTERNAME@hhsnj.org"
+$smtpTo = "Infrastructure@hhsnj.org"
 
 # Define the log file to store the status
 $logFile = "C:\DHCP_Log.txt"
+
 
 # Get the current date and the date from 2 hours ago
 $currentDate = Get-Date
@@ -39,9 +39,20 @@ Get-DhcpServerv4Scope -ComputerName $dhcpServer | ForEach-Object {
         # Send an email notification if the scope is now inactive
         $subject = "DHCP Scope $scopeId is inactive"
         $body = "The DHCP scope $scopeId is currently inactive as of $($currentDate.ToString('yyyy-MM-dd HH:mm:ss'))."
-        Send-MailMessage -To $smtpTo -From $smtpFrom -Subject $subject -Body $body -SmtpServer $smtpServer -Port $smtpPort -Credential $Credential -UseSsl
+        Send-MailMessage -To $smtpTo -From $smtpFrom -Subject $subject -Body $body -SmtpServer $smtpServer
     }
     
     # Write the current status to the log file
     Add-Content -Path $logFile -Value "$scopeId $scopeStatus"
 }
+
+#Run Log clean up
+
+# Read the contents of the text file
+$content = Get-Content $logFile
+
+# Select the last 20 lines
+$newContent = $content | Select-Object -Last 20
+
+# Overwrite the original file with the last 20 lines
+$newContent | Set-Content $logFile
